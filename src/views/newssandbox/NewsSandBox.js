493d1cca1a32a514ea4c22acbb0f2cecb881287f
qiaofugui <903111082@qq.com>
2023-02-16 13:14:12 +0800

    配置路由 引入antd组件库

--- src/views/newssandbox/NewsSandBox.js
+++ src/views/newssandbox/NewsSandBox.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import { Redirect, Route, Switch } from 'react-router-dom'
+
+import SideMenu from '../../components/newssandbox/SideMenu'
+import TopHeader from '../../components/newssandbox/TopHeader'
+
+import Home from './home/Home'
+import UserList from './user-manage/UserList'
+import RoleList from './right-manage/RoleList'
+import RightList from './right-manage/RightList'
+import NoPermission from './nopermission/NoPermission'
 
 export default function NewsSandBox () {
   return (
-    <div>NewsSandBox</div>
+    <div>
+      <SideMenu></SideMenu>
+      <TopHeader></TopHeader>
+
+      <Switch>
+        <Route path='/home' component={Home}></Route>
+        <Route path='/user-manage/list' component={UserList}></Route>
+        <Route path='/right-manage/role/list' component={RoleList}></Route>
+        <Route path='/right-manage/right/list' component={RightList}></Route>
+
+        <Redirect from='/' to='/home' exact />
+        <Route path='*' component={NoPermission}></Route>
+      </Switch>
+    </div>
   )
 }
---
+++ src/views/newssandbox/home/Home.js
@@ -0,0 +1,9 @@
+import { Button } from 'antd'
+
+export default function Home () {
+  return (
+    <div>
+      Home
+    </div>
+  )
+}
