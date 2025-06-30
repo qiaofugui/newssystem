afb212cdfecf13aadbeca957331b95d22be56e30
qiaoronggui <903111082@qq.com>
2025-06-30 10:35:26 +0800

    fix

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,7 +3,8 @@ import axios from "axios";
 import { store } from "../redux/store";
 
 // axios.defaults.baseURL = 'http://0.0.0.0:3001'
-axios.defaults.baseURL = 'http://news.qiaofugui.cn:3001'
+// axios.defaults.baseURL = 'http://news.qiaofugui.cn:3001'
+axios.defaults.baseURL = 'https://news-rear-glitch.glitch.me'
 
 // axios.defaults.headers
 
